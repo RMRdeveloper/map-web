8aea3f5572b3c0fc6b0d3251c4c6225af786b9cc
RMRdeveloper <ronaldmorenorodriguez1990@gmail.com>
2020-10-20 20:21:52 -0400

    Marker circle agregado

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -3,9 +3,21 @@ const d = document;
 d.addEventListener('DOMContentLoaded', (e) => {
 	const socket = io();
 	const map = L.map('map', {
-		center: [18.2375496, -70.2050959],
-		zoom: 9,
+		center: [36.2422994, -113.7487596],
+		zoom: 4,
+		zoomControl: false,
 	});
+	const controlScale = L.control
+		.scale({
+			metric: true,
+		})
+		.addTo(map);
+	const zoomControl = L.control
+		.zoom({
+			zoomInTitle: 'Acercar',
+			zoomOutTitle: 'Alejar',
+		})
+		.addTo(map);
 
 	const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
@@ -20,9 +32,24 @@ d.addEventListener('DOMContentLoaded', (e) => {
 		timeout: 5000,
 	};
 
+	const animateFly = (coords) => {
+		map.flyTo(coords, 13, {
+			animate: true,
+			duration: 0.8,
+		});
+	};
+
+	const createRadius = (coords) => {
+		const circle = L.circle(coords, { radius: 200 })
+			.bindPopup('Alrededor: 200 metros')
+			.addTo(map);
+	};
+
 	const createMarker = (coords) => {
 		const marker = L.marker(coords);
 		marker.bindPopup(`Este usuario está en: ${coords[0]}, ${coords[1]}`).addTo(map);
+		animateFly(coords);
+		createRadius(coords);
 	};
 
 	navigator.geolocation.getCurrentPosition(
@@ -31,8 +58,8 @@ d.addEventListener('DOMContentLoaded', (e) => {
 			createMarker(coords);
 			socket.emit('userCoordinates', coords);
 		},
-		(error) => {
-			alert(`Hubo un error: `, error);
+		(err) => {
+			console.log(`Hubo un error: ${err.message}`);
 		},
 		geoOptions
 	);
